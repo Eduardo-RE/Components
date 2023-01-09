57bed8b600eb81d7692e37657266956bed9efdc3
Eduardo Reyes <Eduardo.reyes@softkitect.com>
2023-01-09 09:34:36 -0800

    Seccion de botones lista

--- src/components/Button/NormalButton.jsx
+++ src/components/Button/NormalButton.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./NormalButton.css";
 
 const NormalButton = () => {
   return (
     <div className="btn-container">
       <button className="btn-normal">Normal Button</button>
+
       <button className="btn-normal btn-transition">Transition Button</button>
-      <button className="btn-normal">Normal Button</button>
-      <button class="btn">Button</button>
-      <div class="dropdown">
-        <button class="btn">
-          <i class="fa fa-caret-down"></i>
+
+      <div className="split-container">
+        <button
+          className="btn-split"
+          style={{ borderRight: "1px solid #b1afaf" }}
+        >
+          Split button
         </button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+        <div className="dropdown">
+          <button className="btn-split">Icon</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
         </div>
+      </div>
+
+      <div className="group-container">
+        <button className="group">Button</button>
+        <button className="group">Group</button>
+        <button className="group">Button</button>
+        <button className="group">Group</button>
       </div>
     </div>
   );
