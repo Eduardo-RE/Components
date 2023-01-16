73c8ace7dcd24ae1c402fc880ad61ec9263001fa
Eduardo Reyes <Eduardo.reyes@softkitect.com>
2023-01-16 14:36:58 -0800

    UI de sidebar terminado

--- src/pages/Comp/SideBar/SideBar.jsx
+++ src/pages/Comp/SideBar/SideBar.jsx
@@ -1,3 +1,12 @@
+import {
+  faCircleArrowLeft,
+  faComment,
+  faCompass,
+  faHome,
+  faTable,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import "./SideBar.css";
@@ -5,22 +14,44 @@ import "./SideBar.css";
 const SideBar = () => {
   return (
     <div className="sidebar-container">
+      <div className="sidebar-logo">Logo</div>
+      <hr class="line-logo" />
       <div className="sideoptions-container">
         <Link to="/">
-          <div>Inicio</div>
+          <div className="button-sidebar-container">
+            <FontAwesomeIcon icon={faHome} style={{ marginRight: "5px" }} />
+            <div className="button-sidebar">Inicio</div>
+          </div>
         </Link>
         <Link to="/login">
-          <div>Login</div>
+          <div className="button-sidebar-container">
+            <FontAwesomeIcon icon={faUser} style={{ marginRight: "5px" }} />
+            <div className="button-sidebar">Login</div>
+          </div>
         </Link>
         <Link to="/crud">
-          <div>CRUD</div>
+          <div className="button-sidebar-container">
+            <FontAwesomeIcon icon={faTable} style={{ marginRight: "5px" }} />
+            <div className="button-sidebar">CRUD</div>
+          </div>
         </Link>
         <Link to="/components">
-          <div>Componentes</div>
+          <div className="button-sidebar-container">
+            <FontAwesomeIcon icon={faCompass} style={{ marginRight: "5px" }} />
+            <div className="button-sidebar">Componentes</div>
+          </div>
         </Link>
         <Link>
-          <div>Blog</div>
+          <div className="button-sidebar-container">
+            <FontAwesomeIcon icon={faComment} style={{ marginRight: "5px" }} />
+            <div className="button-sidebar">Blog</div>
+          </div>
         </Link>
+
+        <div className="deploy-icon">
+          <div className="icon-text">Reducir</div>
+          <FontAwesomeIcon icon={faCircleArrowLeft} />
+        </div>
       </div>
     </div>
   );
