79fcc4c77e2289afbf451636d123258e27759839
Eduardo Reyes <Eduardo.reyes@softkitect.com>
2023-01-17 09:46:25 -0800

    Ya funciona el sidebar pero falta diseño

--- src/pages/Comp/SideBar/SideBar.jsx
+++ src/pages/Comp/SideBar/SideBar.jsx
@@ -2,58 +2,72 @@ import {
   faCircleArrowLeft,
   faComment,
   faCompass,
+  faDragon,
   faHome,
   faTable,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SideBar.css";
 
 const SideBar = () => {
-  return (
-    <div className="sidebar-container">
-      <div className="sidebar-logo">Logo</div>
-      <hr class="line-logo" />
-      <div className="sideoptions-container">
-        <Link to="/">
-          <div className="button-sidebar-container">
-            <FontAwesomeIcon icon={faHome} style={{ marginRight: "5px" }} />
-            <div className="button-sidebar">Inicio</div>
-          </div>
-        </Link>
-        <Link to="/login">
-          <div className="button-sidebar-container">
-            <FontAwesomeIcon icon={faUser} style={{ marginRight: "5px" }} />
-            <div className="button-sidebar">Login</div>
-          </div>
-        </Link>
-        <Link to="/crud">
-          <div className="button-sidebar-container">
-            <FontAwesomeIcon icon={faTable} style={{ marginRight: "5px" }} />
-            <div className="button-sidebar">CRUD</div>
-          </div>
-        </Link>
-        <Link to="/components">
-          <div className="button-sidebar-container">
-            <FontAwesomeIcon icon={faCompass} style={{ marginRight: "5px" }} />
-            <div className="button-sidebar">Componentes</div>
-          </div>
-        </Link>
-        <Link>
-          <div className="button-sidebar-container">
-            <FontAwesomeIcon icon={faComment} style={{ marginRight: "5px" }} />
-            <div className="button-sidebar">Blog</div>
-          </div>
-        </Link>
+  const [navActive, setNavActive] = useState(false);
 
-        <div className="deploy-icon">
-          <div className="icon-text">Reducir</div>
-          <FontAwesomeIcon icon={faCircleArrowLeft} />
+  return (
+    <>
+      <div className={`${navActive ? "desplegado" : ""} sidebar-container`}>
+        <div className="icon-logo">
+          <FontAwesomeIcon icon={faDragon} />
+          <div className="sidebar-logo">APP</div>
+        </div>
+        <hr className="line-logo" />
+        <div className="sideoptions-container">
+          <Link to="/">
+            <div className="button-sidebar-container">
+              <FontAwesomeIcon icon={faHome} style={{ marginRight: "10px" }} />
+              <div className="button-sidebar">Inicio</div>
+            </div>
+          </Link>
+          <Link to="/login">
+            <div className="button-sidebar-container">
+              <FontAwesomeIcon icon={faUser} style={{ marginRight: "10px" }} />
+              <div className="button-sidebar">Login</div>
+            </div>
+          </Link>
+          <Link to="/crud">
+            <div className="button-sidebar-container">
+              <FontAwesomeIcon icon={faTable} style={{ marginRight: "10px" }} />
+              <div className="button-sidebar">CRUD</div>
+            </div>
+          </Link>
+          <Link to="/components">
+            <div className="button-sidebar-container">
+              <FontAwesomeIcon
+                icon={faCompass}
+                style={{ marginRight: "10px" }}
+              />
+              <div className="button-sidebar">Componentes</div>
+            </div>
+          </Link>
+          <Link>
+            <div className="button-sidebar-container">
+              <FontAwesomeIcon
+                icon={faComment}
+                style={{ marginRight: "10px" }}
+              />
+              <div className="button-sidebar">Blog</div>
+            </div>
+          </Link>
+
+          <div className="deploy-icon" onClick={() => setNavActive(!navActive)}>
+            <div className="icon-text">Reducir</div>
+            <FontAwesomeIcon icon={faCircleArrowLeft} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
